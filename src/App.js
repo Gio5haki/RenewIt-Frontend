313cbd6efab222f0095747e963675f608a780d05
Giorgi Shakiashvili <giorgishakiashvili@gmail.com>
2020-05-22 20:06:02 +0400

    added styling to mapChart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactTooltip from "react-tooltip";
 import MapChart from "./components/MapChart";
 //import logo from "./logo.svg";
@@ -8,8 +8,15 @@ function App() {
   const [content, setContent] = useState("");
   return (
     <div>
-     <MapChart setTooltipContent={setContent}></MapChart>
-     <ReactTooltip>{content}</ReactTooltip>
+      <div className="element">Navigation Bar</div>
+      <div className="map-container">
+        <MapChart setTooltipContent={setContent}></MapChart>
+        <ReactTooltip place="top" type="dark" effect="float">
+          {content}
+        </ReactTooltip>
+      </div>
+      <div className="element">Brief Description of the Project</div>
+      <div className="element">Social Media</div>
     </div>
   );
 }
--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,10 +1,5 @@
 import React, { memo } from "react";
-import {
-  ZoomableGroup,
-  ComposableMap,
-  Geographies,
-  Geography,
-} from "react-simple-maps";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
@@ -22,40 +17,44 @@ const rounded = (num) => {
 const MapChart = ({ setTooltipContent }) => {
   return (
     <>
-      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
-        <ZoomableGroup>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onMouseEnter={() => {
-                    const { NAME, POP_EST } = geo.properties;
-                    setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
-                  }}
-                  onMouseLeave={() => {
-                    setTooltipContent("");
-                  }}
-                  style={{
-                    default: {
-                      fill: "#D6D6DA",
-                      outline: "none",
-                    },
-                    hover: {
-                      fill: "#F53",
-                      outline: "none",
-                    },
-                    pressed: {
-                      fill: "#E42",
-                      outline: "none",
-                    },
-                  }}
-                />
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
+      <ComposableMap
+        data-tip=""
+        projectionConfig={{ scale: 150, center: [0, 0] }}
+        width={800}
+        height={400}
+        style={{ width: "100%", height: "auto" }}
+      >
+        <Geographies geography={geoUrl} disableOptimization={true}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                onMouseEnter={() => {
+                  const { NAME, POP_EST } = geo.properties;
+                  setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+                }}
+                onMouseLeave={() => {
+                  setTooltipContent("");
+                }}
+                style={{
+                  default: {
+                    fill: "#D6D6DA",
+                    outline: "none",
+                  },
+                  hover: {
+                    fill: "#53E0EE",
+                    outline: "none",
+                  },
+                  pressed: {
+                    fill: "#E42",
+                    outline: "none",
+                  },
+                }}
+              />
+            ))
+          }
+        </Geographies>
       </ComposableMap>
     </>
   );
