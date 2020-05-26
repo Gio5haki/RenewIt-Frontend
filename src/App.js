3012d9fef5b44779a04874b2a4beb88396987421
Giorgi Shakiashvili <giorgishakiashvili@gmail.com>
2020-05-26 17:54:37 +0400

    created Home view, added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
-import ReactTooltip from "react-tooltip";
-import MapChart from "./components/MapChart";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import Home from "./views/Home";
+
 //import logo from "./logo.svg";
 //import "./App.css";
 
 function App() {
-  const [content, setContent] = useState("");
   return (
-    <div>
-      <div className="element">Navigation Bar</div>
-      <div className="map-container">
-        <MapChart setTooltipContent={setContent}></MapChart>
-        <ReactTooltip place="top" type="dark" effect="float">
-          {content}
-        </ReactTooltip>
-      </div>
+    <div class="App">
+      
+      <Router>
+        <div className="navBar">
+          <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/about">
+            About Section
+          </Route>
+        </Switch>
+      </Router>
+
       <div className="element">Brief Description of the Project</div>
       <div className="element">Social Media</div>
     </div>
---
+++ src/views/Home.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import ReactTooltip from "react-tooltip";
+import MapChart from "../components/MapChart";
+
+function Home() {
+  const [content, setContent] = useState("");
+  return (
+    <div>
+      <div className="map-container">
+        <MapChart setTooltipContent={setContent}></MapChart>
+        <ReactTooltip place="top" type="dark" effect="float">
+          {content}
+        </ReactTooltip>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
