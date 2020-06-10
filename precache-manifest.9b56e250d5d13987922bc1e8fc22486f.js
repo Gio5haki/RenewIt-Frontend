936fa0f35c56635ad5f16bef8323e5ffa9244dc2
Giorgi Shakiashvili <giorgishakiashvili@gmail.com>
2020-06-10 20:05:17 +0400

    Updates

---
+++ precache-manifest.9b56e250d5d13987922bc1e8fc22486f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bf345ff8a7933edfa3074f3168a381",
+    "url": "/renewit-frontend/index.html"
+  },
+  {
+    "revision": "6b7b48e46dd01bdf349d",
+    "url": "/renewit-frontend/static/css/main.6709112b.chunk.css"
+  },
+  {
+    "revision": "c12841c083e0eca8b003",
+    "url": "/renewit-frontend/static/js/2.4d3f9c4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/renewit-frontend/static/js/2.4d3f9c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b7b48e46dd01bdf349d",
+    "url": "/renewit-frontend/static/js/main.2907e0d6.chunk.js"
+  },
+  {
+    "revision": "f0c840461198a12c27f3",
+    "url": "/renewit-frontend/static/js/runtime-main.643bd8c9.js"
+  }
+]);
